339e9c0767eddae017a3629adeb4ea2097739001
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-01-21 06:45:06 +0530

    comment out app.jsx

--- 01_viteReact/src/main.jsx
+++ 01_viteReact/src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './App.jsx'
+// import App from './App.jsx'
 
-function MyApp(){
-    return (
-        <div>
-            <h1>Custom App | chai</h1>
-        </div>
-    )
-}
+// function MyApp(){
+//     return (
+//         <div>
+//             <h1>Custom App | chai</h1>
+//         </div>
+//     )
+// }
 
 // const ReactElement = {
 //     type: 'a',
