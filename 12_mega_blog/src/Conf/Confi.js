44643771a871fe7b01ec67738c6271b2652c8c4a
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-03-26 20:13:27 +0530

    Learn How to make enviroment(.env)

---
+++ 12_mega_blog/src/Conf/Confi.js
@@ -0,0 +1,9 @@
+const Conf = {
+    appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId : String(import.meta.env.VITE_DATABASE_ID),
+    appwriteCollectionId : String(import.meta.env.VITE_COLLECTION_ID),
+    appwriteBucket_Id : String(import.meta.env.VITE_BUCKET_ID),
+}
+
+export default Conf
