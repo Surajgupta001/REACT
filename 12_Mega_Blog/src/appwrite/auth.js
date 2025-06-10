7bcaf1ea1d991e57d73b42aea93c398e7de77efe
Suraj Gupta <surajgupta7070031833@gmail.com>
2025-06-10 22:56:04 +0530

    Build authentication service with appwrite

---
+++ 12_Mega_Blog/src/appwrite/auth.js
@@ -0,0 +1,70 @@
+import config from '../config.js';
+
+import { Client, Account, ID } from 'appwrite';
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(
+                ID.unique(),
+                email,
+                password,
+                name
+            );
+
+            if(userAccount){
+                // call another method to set the user session
+                return this.login({email, password});
+            }
+            else{
+                return userAccount;
+            }
+        }
+        catch (error) {
+            throw new Error(`Error creating account: ${error.message}`);
+        }
+    }
+
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailPasswordSession(email, password);
+        }
+        catch (error) {
+           throw new Error(`Error logging in: ${error.message}`); 
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        }
+        catch (error) {
+            throw new Error(`Error getting current user: ${error.message}`);
+        }
+
+        // return null;
+    }
+    
+    async logout() {
+        try {
+            return await this.account.deleteSessions();
+        }
+        catch (error) {
+            throw new Error(`Error logging out: ${error.message}`);
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService;
